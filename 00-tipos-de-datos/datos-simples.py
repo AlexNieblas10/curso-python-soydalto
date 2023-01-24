d5a57a6ebc3d027b679f2188ff495e2135395055
AlexNieblas <elalexnieblas@gmail.com>
2023-01-24 16:52:11 -0700

    Primeras dos clases del curso

---
+++ 00-tipos-de-datos/datos-simples.py
@@ -0,0 +1,18 @@
+#string en una linea 
+nombre = "Alex" #o 'Alex'
+
+#string en multiples lineas
+apellido = """Nieblas
+Moreno""" #o '''Nieblas Moreno'''
+
+
+#entero/int
+edad = 18
+
+#flotante/float
+pi = 3.1416
+
+
+#booleano
+mayorDeEdad = True
+CredencialDeLector = False
---
+++ 01-variables/variables.py
@@ -0,0 +1,21 @@
+# en python se usa snake_case
+nombre_completo = 'Alex Nieblas'
+
+# variables
+a = 1
+b = 2
+c = a + b
+del c #delete c
+
+# concatenacion
+nombre = 'Alex'
+saludar =  f"Hola {nombre} como estas?"
+
+
+print(saludar) # CTRL + 5 para  ver
+# print(c) Error porque se borra la variable en la linea 5
+
+
+#in or not in
+print("omo" in saludar) # ¿esta la cadena de texto "omo" en saludar?
+print("Pedro" not in saludar) # ¿no esta la cadena de texto "Pedro" en saludar?
